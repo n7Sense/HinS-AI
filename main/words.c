799e0afc552152b5e81e550a23876810543463a7
n7Sense <non2nsense@gmail.com>
2017-08-03 15:14:20 +0530

    1.Return Words
    2. this will return word in a very efficient form

---
+++ main/words.c
@@ -0,0 +1,38 @@
+#include<stdio.h>  
+#include<conio.h>  
+void main(){  
+
+	FILE *fp;  
+	char c;  
+	int i=0, k=0, words = 0, fileLength = 0;
+	int cur=0;
+
+	fp=fopen("Domain.ns","r");
+	fileLength = ftell(fp);
+
+	if(fp == NULL){
+		printf("File does not exist\n");
+	}
+
+	while((c=fgetc(fp))!=EOF){
+
+		if(c==' ' || c=='\n' || c == '\t'){
+			i++;
+			if((cur+1)==i)
+				words++;
+			//printf("%d", i);
+		}else{
+			cur = i;
+
+		}
+
+		if(c=='\n'){
+			k++;
+		}
+	}
+
+	printf("\nThe total No, Of words in file is %d", words+1);
+	printf("\nThe total No, Of lines in file is %d", k+1);
+	printf("\nFile Length Is %d", fileLength);
+	fclose(fp);   
+}  
