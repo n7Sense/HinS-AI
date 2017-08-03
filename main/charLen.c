79ee82db20ccd3849d4442749c629dd339711f3c
n7Sense <non2nsense@gmail.com>
2017-08-03 15:13:34 +0530

    1. Words and Char Length
    1.Write a program that return Char Length and assigned into array of character

---
+++ main/charLen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>  
+#include <conio.h>  
+
+void main (){
+   
+   FILE *fp;  
+   int charLength;  
+   char ch;
+   int i=0, k=0;
+   fp = fopen("Domain.ns", "r");
+   fseek(fp, 0, SEEK_END);  
+   charLength = ftell(fp)+1;  
+   fclose(fp);
+
+   char stream[charLength];
+   fp = fopen("Domain.ns", "r");
+
+   while((ch=fgetc(fp))!=EOF){ 
+   		stream[i] = ch;
+   		i++;
+	}
+	fclose(fp);
+	while(k<=sizeof(stream)-1){
+		printf("%c", stream[k]);
+		k++;
+	}
+	
+//	printf("\n%d", sizeof(stream));
+   printf("\nSize of file: %d bytes", charLength);
+}
